f89864d056c33e20ba323a86fb25b3c961994794
erayaras <erayaras@msn.com>
2023-10-08 20:03:29 +0300

    Fix BubbleSheet proptypes

--- src/modules/QuestionBank/components/organisms/BubbleSheet/BubbleSheet.component.js
+++ src/modules/QuestionBank/components/organisms/BubbleSheet/BubbleSheet.component.js
@@ -33,11 +33,21 @@ const BubbleSheet = ({
 BubbleSheet.propTypes = {
   questions: PropTypes.arrayOf(
     PropTypes.shape({
-      number: PropTypes.number.isRequired,
-      options: PropTypes.arrayOf(PropTypes.string).isRequired,
-      selectedOption: PropTypes.string,
+      id: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      questionText: PropTypes.string.isRequired,
+      options: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          text: PropTypes.string.isRequired,
+        })
+      ).isRequired,
+      correctAnswerId: PropTypes.string.isRequired,
     })
   ).isRequired,
+  lessonName: PropTypes.string.isRequired,
+  selectedOptions: PropTypes.object.isRequired,
+  currentQuestionNumber: PropTypes.number.isRequired,
 }
 
 export default BubbleSheet
